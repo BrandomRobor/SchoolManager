429767aeee74e85c7e39de017ffd3e75ab9d0b4c
Brandom Rodriguez <brandomrobor@tuta.io>
2021-04-01 22:45:13 -0700

    Added navigation graph and added container view to main activity

--- app/src/main/java/me/brandom/schoolmanager/ui/MainActivity.kt
+++ app/src/main/java/me/brandom/schoolmanager/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package me.brandom.schoolmanager
+package me.brandom.schoolmanager.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import me.brandom.schoolmanager.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
