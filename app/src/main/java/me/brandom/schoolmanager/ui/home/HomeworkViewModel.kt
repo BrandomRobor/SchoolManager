05996b16e5f2f23d1aa0368653c975b761d56f99
Brandom Rodriguez <brandomrobor@tuta.io>
2021-04-01 22:53:02 -0700

    Added state management for homework data on ViewModel

---
+++ app/src/main/java/me/brandom/schoolmanager/ui/home/HomeworkViewModel.kt
@@ -0,0 +1,39 @@
+package me.brandom.schoolmanager.ui.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import me.brandom.schoolmanager.database.daos.HomeworkDao
+import me.brandom.schoolmanager.database.entities.SubjectWithHomeworks
+import javax.inject.Inject
+
+@HiltViewModel
+@ExperimentalCoroutinesApi
+class HomeworkViewModel @Inject constructor(private val homeworkDao: HomeworkDao) : ViewModel() {
+    private val _retrievalState =
+        MutableStateFlow<HomeworkRetrievalState>(HomeworkRetrievalState.Loading)
+    val retrievalState: StateFlow<HomeworkRetrievalState> = _retrievalState
+
+    init {
+        viewModelScope.launch {
+            try {
+                homeworkDao.getAllSubjectsWithHomework().collect {
+                    _retrievalState.value = HomeworkRetrievalState.Success(it)
+                }
+            } catch (e: Exception) {
+                _retrievalState.value = HomeworkRetrievalState.Error
+            }
+        }
+    }
+
+    sealed class HomeworkRetrievalState {
+        object Loading : HomeworkRetrievalState()
+        object Error : HomeworkRetrievalState()
+        data class Success(val homeworkList: List<SubjectWithHomeworks>) : HomeworkRetrievalState()
+    }
+}
