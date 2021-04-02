d0447ccab7b8dfe76c128ca83fd9fb460d79cb86
Brandom Rodriguez <brandomrobor@tuta.io>
2021-04-01 20:12:40 -0700

    Create homework DAO

---
+++ app/src/main/java/me/brandom/schoolmanager/database/daos/HomeworkDao.kt
@@ -0,0 +1,30 @@
+package me.brandom.schoolmanager.database.daos
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Transaction
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
+import me.brandom.schoolmanager.database.entities.Homework
+import me.brandom.schoolmanager.database.entities.SubjectWithHomeworks
+
+@Dao
+interface HomeworkDao {
+    @Insert
+    suspend fun insertHomework(homework: Homework)
+
+    @Update
+    suspend fun updateHomework(homework: Homework)
+
+    @Delete
+    suspend fun deleteHomework(homework: Homework)
+
+    @Transaction
+    @Query("SELECT * FROM subject")
+    fun getAllSubjectsWithHomework(): Flow<List<SubjectWithHomeworks>>
+
+    @Query("SELECT * FROM homework WHERE id = :id")
+    fun getHomeworkById(id: Int): Flow<Homework>
+}
