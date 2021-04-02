d922d041aca8ed967a4bde70e4880e9ad7c62db5
Brandom Rodriguez <brandomrobor@tuta.io>
2021-04-01 23:23:54 -0700

    Added Material toolbar to activity Also activity is now using view binding

--- app/src/main/java/me/brandom/schoolmanager/ui/MainActivity.kt
+++ app/src/main/java/me/brandom/schoolmanager/ui/MainActivity.kt
@@ -2,11 +2,16 @@ package me.brandom.schoolmanager.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import me.brandom.schoolmanager.R
+import dagger.hilt.android.AndroidEntryPoint
+import me.brandom.schoolmanager.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
