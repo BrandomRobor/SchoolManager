0a2cb5d587d11e6541ecf0ac1b3539829daba33f
Brandom Rodriguez <brandomrobor@tuta.io>
2021-04-01 18:54:12 -0700

    Create subject entity

---
+++ app/src/main/java/me/brandom/schoolmanager/database/entities/Subject.kt
@@ -0,0 +1,13 @@
+package me.brandom.schoolmanager.database.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class Subject(
+    val name: String,
+    val location: String? = null,
+    val teacherName: String? = null,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0
+)
