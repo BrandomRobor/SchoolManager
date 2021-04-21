b5656baf31e748908c3adbbcea97c7decd9ca480
Brandom Rodriguez <brandomrobor@tuta.io>
2021-04-21 16:33:16 -0600

    Added code to get all subjects and display them on Subjects fragment

--- app/src/main/java/me/brandom/schoolmanager/ui/subject/SubjectFragment.kt
+++ app/src/main/java/me/brandom/schoolmanager/ui/subject/SubjectFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
 import me.brandom.schoolmanager.databinding.FragmentSubjectBinding
 
+@AndroidEntryPoint
 class SubjectFragment : Fragment() {
     private var _binding: FragmentSubjectBinding? = null
     private val binding get() = _binding!!
+    private val viewModel by viewModels<SubjectViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -18,6 +25,29 @@ class SubjectFragment : Fragment() {
     ): View {
         _binding = FragmentSubjectBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val adapter = SubjectListAdapter()
+
+        binding.apply {
+            fragmentSubjectRecyclerView.setHasFixedSize(true)
+            fragmentSubjectRecyclerView.adapter = adapter
+
+            viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+                viewModel.retrievalState.collect {
+                    if (it is SubjectViewModel.SubjectRetrievalState.Success) {
+                        if (it.subjectList.isEmpty()) {
+                            fragmentSubjectRecyclerView.isVisible = false
+                        } else {
+                            fragmentSubjectRecyclerView.isVisible = true
+                            adapter.submitList(it.subjectList)
+                        }
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
---
+++ app/src/main/java/me/brandom/schoolmanager/ui/subject/SubjectViewModel.kt
@@ -0,0 +1,32 @@
+package me.brandom.schoolmanager.ui.subject
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import me.brandom.schoolmanager.database.daos.SubjectDao
+import me.brandom.schoolmanager.database.entities.Subject
+import javax.inject.Inject
+
+@HiltViewModel
+class SubjectViewModel @Inject constructor(private val subjectDao: SubjectDao) : ViewModel() {
+    private val _retrievalState =
+        MutableStateFlow<SubjectRetrievalState>(SubjectRetrievalState.Loading)
+    val retrievalState: StateFlow<SubjectRetrievalState> = _retrievalState
+
+    init {
+        viewModelScope.launch {
+            subjectDao.getAllSubjects().collect {
+                _retrievalState.value = SubjectRetrievalState.Success(it)
+            }
+        }
+    }
+
+    sealed class SubjectRetrievalState {
+        object Loading : SubjectRetrievalState()
+        data class Success(val subjectList: List<Subject>) : SubjectRetrievalState()
+    }
+}
