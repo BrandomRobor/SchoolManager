1ded26bee377fc0623f33922cbc528cdfa7fc739
Brandom Rodriguez <brandomrobor@tuta.io>
2021-04-21 14:44:45 -0600

    Removed dumb code to check if any homework exist

--- app/src/main/java/me/brandom/schoolmanager/ui/home/HomeworkViewModel.kt
+++ app/src/main/java/me/brandom/schoolmanager/ui/home/HomeworkViewModel.kt
@@ -22,22 +22,14 @@ class HomeworkViewModel @Inject constructor(private val homeworkDao: HomeworkDao
     init {
         viewModelScope.launch {
             homeworkDao.getAllSubjectsWithHomework().collect {
-                var counter = 0
-                it.forEach { subject ->
-                    counter += subject.homework.size
-                }
-
-                _retrievalState.value = HomeworkRetrievalState.Success(it, counter > 0)
+                _retrievalState.value =
+                    HomeworkRetrievalState.Success(it)
             }
         }
     }
 
     sealed class HomeworkRetrievalState {
         object Loading : HomeworkRetrievalState()
-        data class Success(
-            val homeworkList: List<SubjectWithHomeworks>,
-            val homeworkExist: Boolean
-        ) :
-            HomeworkRetrievalState()
+        data class Success(val homeworkList: List<SubjectWithHomeworks>) : HomeworkRetrievalState()
     }
 }
