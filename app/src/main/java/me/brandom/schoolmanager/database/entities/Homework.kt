c52dc6f970b79a7f1401cf3bbd539b82922079ae
Brandom Rodriguez <brandomrobor@tuta.io>
2021-05-04 16:17:10 -0600

    Added parcelize plugin and made Homework class parcelable

--- app/src/main/java/me/brandom/schoolmanager/database/entities/Homework.kt
+++ app/src/main/java/me/brandom/schoolmanager/database/entities/Homework.kt
@@ -1,9 +1,11 @@
 package me.brandom.schoolmanager.database.entities
 
+import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
 @Entity(
     foreignKeys = [ForeignKey(
@@ -12,6 +14,7 @@ import androidx.room.PrimaryKey
         childColumns = ["subjectId"]
     )]
 )
+@Parcelize
 data class Homework(
     val hwName: String,
     val deadline: Long,
@@ -20,4 +23,4 @@ data class Homework(
     val description: String? = null,
     @PrimaryKey(autoGenerate = true)
     val hwId: Int = 0
-)
+) : Parcelable
