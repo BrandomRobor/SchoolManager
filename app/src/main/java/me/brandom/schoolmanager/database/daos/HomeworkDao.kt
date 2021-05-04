c9815916b6e51b53da96eb48ca8c927be912d695
Brandom Rodriguez <brandomrobor@tuta.io>
2021-05-04 16:29:07 -0600

    Removed old relationship entity No longer needed

--- app/src/main/java/me/brandom/schoolmanager/database/daos/HomeworkDao.kt
+++ app/src/main/java/me/brandom/schoolmanager/database/daos/HomeworkDao.kt
@@ -4,12 +4,10 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Transaction
 import androidx.room.Update
 import kotlinx.coroutines.flow.Flow
 import me.brandom.schoolmanager.database.entities.Homework
 import me.brandom.schoolmanager.database.entities.HomeworkWithSubject
-import me.brandom.schoolmanager.database.entities.SubjectWithHomeworks
 
 @Dao
 interface HomeworkDao {
@@ -21,10 +19,6 @@ interface HomeworkDao {
 
     @Delete
     suspend fun deleteHomework(homework: Homework)
-
-    @Transaction
-    @Query("SELECT * FROM subject")
-    fun getAllSubjectsWithHomework(): Flow<List<SubjectWithHomeworks>>
 
     @Query("SELECT * FROM homework WHERE hwId = :id")
     fun getHomeworkById(id: Int): Flow<Homework>
