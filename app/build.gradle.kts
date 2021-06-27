6926941e543f761e40f410d116d712bcf46e4f41
Brandom Rodriguez <brandomrobor@tuta.io>
2021-06-27 12:29:44 -0600

    Updated Kotlin to 1.5.20 Also forgot to update Hilt plugin version. Oops.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.10")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.20")
     implementation("androidx.core:core-ktx:1.5.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
