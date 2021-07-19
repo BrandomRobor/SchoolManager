c2377c8703f78688942b7f54ee2922c78168c260
Brandom Rodriguez <brandomrobor@tuta.io>
2021-07-19 17:59:02 -0600

    Created notification channel Required to notify on devices that use Android 8 and above

--- app/src/main/java/me/brandom/schoolmanager/SchoolManagerApplication.kt
+++ app/src/main/java/me/brandom/schoolmanager/SchoolManagerApplication.kt
@@ -1,7 +1,23 @@
 package me.brandom.schoolmanager
 
 import android.app.Application
+import androidx.core.app.NotificationChannelCompat
+import androidx.core.app.NotificationManagerCompat
 import dagger.hilt.android.HiltAndroidApp
+import me.brandom.schoolmanager.utils.NotificationChannelIds
 
 @HiltAndroidApp
-class SchoolManagerApplication : Application()
+class SchoolManagerApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val reminderChannel = NotificationChannelCompat.Builder(
+            NotificationChannelIds.HOMEWORK_REMINDER,
+            NotificationManagerCompat.IMPORTANCE_HIGH
+        )
+            .setName("Homework reminders")
+            .build()
+
+        NotificationManagerCompat.from(applicationContext)
+            .createNotificationChannel(reminderChannel)
+    }
+}
