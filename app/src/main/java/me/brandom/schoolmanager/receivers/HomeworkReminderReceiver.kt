2f50a4ed545761b0f6c9be4909c4ba652b5cb408
Brandom Rodriguez <brandomrobor@tuta.io>
2021-07-21 20:45:53 -0600

    Added subject name of the homework on the notification Also found a bug with how we pass the homework to the receiver. Working on that now

--- app/src/main/java/me/brandom/schoolmanager/receivers/HomeworkReminderReceiver.kt
+++ app/src/main/java/me/brandom/schoolmanager/receivers/HomeworkReminderReceiver.kt
@@ -5,22 +5,44 @@ import android.content.Context
 import android.content.Intent
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import me.brandom.schoolmanager.R
+import me.brandom.schoolmanager.database.daos.SubjectDao
 import me.brandom.schoolmanager.database.entities.Homework
+import me.brandom.schoolmanager.utils.ApplicationScope
 import me.brandom.schoolmanager.utils.NotificationChannelIds
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class HomeworkReminderReceiver : BroadcastReceiver() {
+    @Inject
+    lateinit var subjectDao: SubjectDao
+
+    @Inject
+    @ApplicationScope
+    lateinit var applicationScope: CoroutineScope
+
     override fun onReceive(context: Context, intent: Intent) {
         val bundle = intent.getBundleExtra("bundle")!!
         val homework = bundle.getParcelable<Homework>("homework")!!
+        val async = goAsync()
 
-        val notification =
-            NotificationCompat.Builder(context, NotificationChannelIds.HOMEWORK_REMINDER)
-                .setContentTitle(homework.hwName)
-                .setSmallIcon(R.drawable.ic_book)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .build()
+        applicationScope.launch {
+            val dbSubject = subjectDao.getSubjectById(homework.subjectId).first()
 
-        NotificationManagerCompat.from(context).notify(homework.hwId, notification)
+            val notification =
+                NotificationCompat.Builder(context, NotificationChannelIds.HOMEWORK_REMINDER)
+                    .setContentTitle(homework.hwName)
+                    .setContentText(dbSubject.name)
+                    .setSmallIcon(R.drawable.ic_book)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .build()
+
+            NotificationManagerCompat.from(context).notify(homework.hwId, notification)
+            async.finish()
+        }
     }
 }
