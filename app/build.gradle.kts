5bb1c320d9a0bb888d42ed2b01116a28d1313e13
Brandom Rodriguez <brandomrobor@tuta.io>
2021-08-24 18:00:00 -0600

    Updated dependencies and Gradle plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("com.google.android.material:material:1.4.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.0.4")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
@@ -94,7 +94,7 @@ dependencies {
     implementation("androidx.preference:preference-ktx:1.1.1")
 
     // DataStore (to save preferences)
-    implementation("androidx.datastore:datastore-preferences:1.0.0-rc02")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 }
 
 kapt {
