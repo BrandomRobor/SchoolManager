c399b2cb9c9840053e8eba9998edf9a7db0b9c22
Brandom Rodriguez <brandomrobor@tuta.io>
2021-08-25 10:20:23 -0600

    Created new layout for homework items This new layout is more inline with material guidelines Removed the description line as it will be displayed in a details fragment later on

--- app/src/main/java/me/brandom/schoolmanager/ui/home/HomeworkListAdapter.kt
+++ app/src/main/java/me/brandom/schoolmanager/ui/home/HomeworkListAdapter.kt
@@ -2,12 +2,9 @@ package me.brandom.schoolmanager.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.appcompat.widget.PopupMenu
-import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import me.brandom.schoolmanager.R
 import me.brandom.schoolmanager.database.entities.Homework
 import me.brandom.schoolmanager.database.entities.HomeworkWithSubject
 import me.brandom.schoolmanager.database.entities.Subject
@@ -35,32 +32,6 @@ class HomeworkListAdapter(val homeworkManager: HomeworkManager) :
         RecyclerView.ViewHolder(binding.root) {
         init {
             binding.apply {
-                val menu = PopupMenu(root.context, itemHomeworkMenu)
-                menu.inflate(R.menu.homework_menu)
-
-                itemHomeworkMenu.setOnClickListener {
-                    menu.show()
-                }
-
-                menu.setOnMenuItemClickListener {
-                    if (adapterPosition != RecyclerView.NO_POSITION) {
-                        val homework = getItem(adapterPosition).homework
-                        when (it.itemId) {
-                            R.id.homework_edit_item -> {
-                                homeworkManager.editHomework(homework)
-                                true
-                            }
-                            R.id.homework_delete_item -> {
-                                homeworkManager.deleteHomework(homework)
-                                true
-                            }
-                            else -> false
-                        }
-                    } else {
-                        false
-                    }
-                }
-
                 itemHomeworkCheckBox.setOnCheckedChangeListener { _, isChecked ->
                     val homework = getItem(adapterPosition).homework
                     homeworkManager.markAsCompleted(homework, isChecked)
@@ -71,14 +42,8 @@ class HomeworkListAdapter(val homeworkManager: HomeworkManager) :
         fun bind(homework: Homework, subject: Subject) {
             binding.apply {
                 itemHomeworkName.text = homework.hwName
-
-                itemHomeworkDescription.isVisible = homework.description != null
-                itemHomeworkDescription.text = homework.description
-
                 itemHomeworkDeadline.text = homework.formattedDateTime
-
                 itemHomeworkSubject.text = subject.name
-
                 itemHomeworkCheckBox.isChecked = homework.isComplete
             }
         }
