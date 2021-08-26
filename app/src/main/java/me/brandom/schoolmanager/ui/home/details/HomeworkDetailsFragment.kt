a347d8ee28a613480d2f62b71bcd40e09f838fad
Brandom Rodriguez <brandomrobor@tuta.io>
2021-08-26 11:28:16 -0600

    Created details fragment

---
+++ app/src/main/java/me/brandom/schoolmanager/ui/home/details/HomeworkDetailsFragment.kt
@@ -0,0 +1,29 @@
+package me.brandom.schoolmanager.ui.home.details
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+import me.brandom.schoolmanager.databinding.FragmentHomeworkDetailsBinding
+
+@AndroidEntryPoint
+class HomeworkDetailsFragment : Fragment() {
+    private var _binding: FragmentHomeworkDetailsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeworkDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
